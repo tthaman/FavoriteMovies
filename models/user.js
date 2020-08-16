a7baf1ee8c28739a7d83b1e6deb13a602b32ccb7
Andrew Weiss <wandrew8@gmail.com>
2020-08-16 15:55:24 -0700

    adds model schemas for movies, revies, saved, and users

--- models/user.js
+++ models/user.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  password:  { type: String, required: true},
-  email: { type: String, unique: true, required: true},
+  password:  { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  avatar: { type: String },
+  reviews: [{ type: Schema.Types.ObjectId, ref: 'reviews' }],
   roles: { type: [String], required: true }
 });
 
