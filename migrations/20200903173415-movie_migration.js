c5733bb3955d059bbaa61f2ea4f3fb408c3b341f
sashako8 <korena@uw.edu>
2020-09-07 14:18:16 -0700

    changed signup redirect to login

--- migrations/20200903173415-movie_migration.js
+++ migrations/20200903173415-movie_migration.js
@@ -4,11 +4,16 @@ module.exports = {
     // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
     // Example:
     // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
-  },
+    await db.movies.find().forEach((movie) => {
+      db.movies.update( { $set: { averageRating: (parsefloat(movie.RottenTomatoes) + movie.IMDb*100)/2 } });
+      });
+    },
 
   async down(db, client) {
     // TODO write the statements to rollback your migration (if possible)
     // Example:
     // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
-  }
+    await db.movies.updateMany(
+       { $unset: { averageRating: "" } });
+  },
 };
