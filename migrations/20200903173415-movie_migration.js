714c33a31d2a709659ca084a1f240d89a9a18b29
sashako8 <korena@uw.edu>
2020-09-12 11:20:58 -0700

    added down migration

--- migrations/20200903173415-movie_migration.js
+++ migrations/20200903173415-movie_migration.js
@@ -4,10 +4,9 @@ module.exports = {
     // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
     // Example:
     // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
-    await db.collection('movies').updateMany({},
-      { $match: { Runtime: { $ne: "" } } },
+    await db.collection('movies').find({ Runtime: { $ne: "" } },
       { $set: { Runtime: { $concat: [{ $toString: { $floor: { $divide: ["$Runtime", 60] } } }, "h ", { $toString: { $mod: ["$Runtime", 60] } }, "m"] } } },
-      { multi: true }
+      { multi: true },
     )
   },
 
@@ -15,5 +14,9 @@ module.exports = {
     // TODO write the statements to rollback your migration (if possible)
     // Example:
     // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+    await db.collection('movies').find({ Runtime: { $ne: "" } },
+      { $set: { Runtime3: { $add: [{ $multiply: [{ $toInt: { $trim: { input: { $arrayElemAt: [{ $split: [ "$Runtime2" , " "]}, 0 ] }, chars: "h" } } }, 60] }, { $toInt: {$trim: { input: { $arrayElemAt: [{ $split: [ "$Runtime2" , " "]}, 1 ] }, chars: "m" } } }] } } },
+      { multi: true },
+    )
   }
 };
